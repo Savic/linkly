756640cc048b29353b18e0639c7f854288fdd043
Zeljko Savic <91739500+ZeljkoSavicMiracle@users.noreply.github.com>
2024-04-20 19:35:12 +0200

    - Add of project baseline

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,16 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import "./styles/style.css";
-
-
 
 const App = () => (
     <div className="p-4">
         <h1 className="text-2xl font-bold">Hello, React with Tailwind and Shad UI!</h1>
-        <h1 className="text-2xl font-bold">Hello, React with Tailwind and Shad UI! asdfs</h1>
-
-        adsf
     </div>
 );
 
-ReactDOM.render(<App/>, document.getElementById("root"));
+const root: HTMLElement = document.getElementById("root") as HTMLElement;
+const rootElement = createRoot(root);
+rootElement.render(<App />);

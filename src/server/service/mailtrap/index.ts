3dce33787a5cbca2f620b8971ed3987b8e3cef4c
Zeljko Savic <91739500+ZeljkoSavicMiracle@users.noreply.github.com>
2024-04-21 23:01:24 +0200

    - Add of simple mailtrap service class

---
+++ src/server/service/mailtrap/index.ts
@@ -0,0 +1,62 @@
+import {MailtrapClient, SendResponse} from "mailtrap";
+
+/**
+ * Represents a singleton EmailService.
+ * @constructor
+ */
+class EmailService {
+    private static instance: EmailService;
+    private client: MailtrapClient;
+
+    private constructor() {
+        const token = "84815e32cdc4d74effa7184e5e6a23ba";
+
+        this.client = new MailtrapClient({token});
+    }
+
+    static getInstance(): EmailService {
+        if (!EmailService.instance) {
+            EmailService.instance = new EmailService();
+        }
+
+        return EmailService.instance;
+    }
+
+    /**
+     * Sends an email with the given subject, text, category, recipient email, and recipient name.
+     *
+     * @param {string} subject - The subject of the email.
+     * @param {string} text - The text content of the email.
+     * @param {string} category - The category of the email.
+     * @param {string} recipientEmail - The email address of the recipient.
+     * @param {string} recipientName - The name of the recipient.
+     * @return {Promise<SendResponse>} - A promise that resolves with the response from the email sending operation.
+     */
+    sendEmail(
+        subject: string,
+        text: string,
+        category: string,
+        recipientEmail: string,
+        recipientName: string
+    ): Promise<SendResponse> {
+        const sender = {
+            email: "mailtrap@linkly.space",
+            name: recipientName,
+        };
+
+        const recipients = [
+            {
+                email: recipientEmail,
+            }
+        ];
+
+        return this.client
+            .send({
+                from: sender,
+                to: recipients,
+                subject: subject,
+                text: text,
+                category: category,
+            });
+    }
+}
